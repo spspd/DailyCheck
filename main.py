7b8e183952bd2b33f33cae216c1e2220023bcb0a
Gyu min Kim <gyuminkim@Gyuui-MacBookAir.local>
2022-05-16 10:57:48 +0900

    sync

---
+++ main.py
@@ -0,0 +1,13 @@
+try:
+    import Tkinter as tk
+except:
+    import tkinter as tk
+    
+
+    
+app = tk.Tk()
+app.title("configure method")
+app.geometry('300x200')
+
+app.configure(bg='red')
+app.mainloop()
